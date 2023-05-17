8010cf120ce19f671466bb9847c75cca2fea7ff3
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2023-05-18 03:38:52 +0530

    feat: frontend code setup

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,31 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import allRoutes from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const CLIENT_URL_STRING = process.env.CLIENT_URL || "http://localhost:3000";
 
+const allowedDomains = CLIENT_URL_STRING.split(", ");
+
+// middleware
+app.use(
+  cors({
+    credentials: true,
+    origin(origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+
+      if (allowedDomains.indexOf(origin) === -1) {
+        const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser());
@@ -16,8 +36,7 @@ app.use(cookieParser());
 app.use("/api", allRoutes);
 
 // error handler
-app.use((err, req, res, next) => {
-  console.log({ err });
+app.use((err, req, res) => {
   const status = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   res.status(status).json({ message });

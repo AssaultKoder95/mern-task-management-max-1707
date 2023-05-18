43b8f20a881750bf36965d1d6c048d1ad7041976
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2023-05-18 18:45:55 +0530

    feat: complete cron job code

--- backend/cron/index.js
+++ backend/cron/index.js
@@ -1,16 +1,60 @@
-import Reminder from "../models/Reminder.js";
 import cron from "node-cron";
+import Reminder from "../models/Reminder.js";
+import { connectDB } from "../index.js";
+import sendEmail from "../utils/emailSender.js";
 
-cron.schedule("* * * * *", async function () {
-  console.log("cron being called");
+cron.schedule("* * * * *", async () => {
+  try {
+    await connectDB();
 
-  const reminders = await Reminder.find({
-    sent: false,
-    $lte: new Date(),
-  }).exec();
+    const reminders = await Reminder.find({
+      sent: false,
+    }).populate({
+      path: "task",
+      populate: [
+        {
+          path: "user",
+        },
+      ],
+    });
 
-  // send emails to these users of these reminders
+    console.log(`Successfully fetched ${reminders.length} reminders from DB.`);
 
-  const taskIds = reminders.map(({ task }) => task.id);
-  await Reminder.updateMany({ task: { $in: taskIds } }, { sent: true });
+    const remindersPayload = reminders.map(
+      ({
+        task: {
+          user: { name, email },
+          title,
+          dueDate,
+        },
+        remindOn,
+        _id,
+      }) => ({
+        email,
+        name,
+        remindOn,
+        dueDate,
+        title,
+        reminderId: _id,
+      })
+    );
+
+    const sendingEmailPromises = remindersPayload.map(
+      ({ title, email, dueDate, name }) =>
+        sendEmail({
+          subject: `reminder for task - ${title}`,
+          to: email,
+          text: `Hey ${name}, A friendly reminder about your task - ${title} pending on ${dueDate}.`,
+        })
+    );
+
+    await Promise.allSettled(sendingEmailPromises);
+    console.log("Successfully sent email to all recipients.");
+
+    const reminderIds = remindersPayload.map(({ reminderId }) => reminderId);
+    await Reminder.updateMany({ _id: { $in: reminderIds } }, { sent: true });
+    console.log("Successfully updated status in DB.");
+  } catch (error) {
+    console.log(error);
+  }
 });
